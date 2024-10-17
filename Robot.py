0750353efc06207f096331547c18387b4add0c86
aaronforsnow <twodoschool@gmail.com>
2024-10-17 17:02:07 -0600

    ready to code robot

---
+++ Robot.py
@@ -0,0 +1,5 @@
+# this is where the robot will have a tree and be given a list of ISBN's to find 
+# and calulate the cost of collecting the books
+class Robot:
+    def __init__(self):
+        return
---
+++ Robot_tests.py
@@ -0,0 +1,17 @@
+from Robot import Robot;
+from Tree import tree;
+from Bin import Bin;
+
+test_tree = tree(
+    #     LL   L  LRL  LR   0  RL   R    
+        [123, 12,  23, 45, 56, 67, 78], #ISBN
+        [  1,  7,   3,  4,  7,  2,  9]  #Frequency 33 = total  split 23 and 11 
+)
+
+def test_robot_can_give_cost_single_book():
+    #move to root (1)  cost + 1
+    #scan              cost + .5
+    #retrive book      cost + .25
+    #change direction  cost + 10
+    #go back           cost + 1
+    assert False
