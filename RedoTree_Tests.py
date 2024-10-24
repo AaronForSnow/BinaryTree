f64068a7f3b38780f9e4d7a17195a79af54d081a
reall <reall@MSI>
2024-10-24 14:53:41 -0600

    base case of only 1 isbn passes

--- RedoTree_Tests.py
+++ RedoTree_Tests.py
@@ -20,3 +20,14 @@ def test_can_create_tree_with_only_one_isbn():
     assert my_tree.root_bin.Isbn == 12
     assert my_tree.root_bin.Left == None
     assert my_tree.root_bin.Right == None
+
+def test_can_create_tree_with_only_one_isbn():
+    my_tree = RedoTree(
+            [12, 13], #ISBN
+            [2, 37]  #Frequency
+    )
+    assert my_tree.root_bin.Frequency == 37
+    assert my_tree.root_bin.Isbn == 13
+    assert my_tree.root_bin.Left.Isbn == 12
+    assert my_tree.root_bin.Left.Frequency == 2
+    assert my_tree.root_bin.Right == None
